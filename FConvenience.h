3bbbbb56292e6e03770734e597fc8e08e428b673
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2013-01-06 19:36:00 +0900

    Added a few shorthands and tweaked logger

--- FConvenience.h
+++ FConvenience.h
@@ -1,7 +1,6 @@
-#import <Foundation/Foundation.h>
-
 // Frequently used macros for uncluttering things (Import in your PCH)
-#define Log(...) fprintf(stderr, "%s[%u] %s: %s\n", \
+
+#define _Log(prefix, ...) fprintf(stderr, prefix "%s[%u] %s: %s\n", \
     [[[NSProcessInfo processInfo] processName] UTF8String], \
     getpid(),\
     [[NSString stringWithFormat:@"%10.15s:%u", \
@@ -9,10 +8,11 @@
                                 __LINE__] UTF8String],\
     [[NSString stringWithFormat:__VA_ARGS__] UTF8String])
 
+#define Log(...) _Log("V ", ##__VA_ARGS__) // V: Verbose
+
 #ifdef DEBUG
     #define CrashHere() { *(int *)0 = 0xDEADBEEF; }
-    // We make it a warning because when unit testing it's nice to see the logs in the list view instead of having to scroll through all the compiler output
-    #define DebugLog(...) Log(@"Warning: " __VA_ARGS__)
+    #define DebugLog(...) _Log("D ", ##__VA_ARGS__) // D: Debug
 #else
     #define CrashHere()
     #define DebugLog(...) 
@@ -25,6 +25,14 @@
 #define Async(...) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ##__VA_ARGS__)
 #define AsyncOnMain(...) dispatch_async(dispatch_get_main_queue(), ##__VA_ARGS__)
 
+#define Workspace [NSWorkspace sharedWorkspace]
+#define FileManager [NSFileManager defaultManager]
+
+// iOS/Mac specific
 #if TARGET_OS_IPHONE
-#define Animate(dur, ...) [UIView animateWithDuration:(dur) animations:__VA_ARGS__]
+    #define Animate(dur, ...) [UIView animateWithDuration:(dur) animations:__VA_ARGS__]
+    #define Device [UIDevice currentDevice]
+    #define UIApp [UIApplication sharedApplication]
+#else
+    #define Defaults [NSUserDefaults standardUserDefaults]
 #endif
